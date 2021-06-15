138b0cd4f9098604bfef7e520b687b7370c6aac0
Mian Jawad Ahmad <a.jawad321@gmail.com>
2021-06-15 12:39:07 +0500

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -51,3 +51,31 @@ class Blockchain:
             previous_block = block 
             block_index += 1
         return True
+    
+    
+#create web app
+
+app = Flask(__name__)
+
+#create blockchain 
+blockchain = Blockchain()
+
+#mining a new block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'congratulations, you just mine a block!',
+                'index' : block['index'],
+                'timestamp' : block['timestamp'],
+                'proof' : block['proof'],
+                'previous_hash' : block['previous_hash']}
+    return jsonify(response), 200
+
+
+
+
+
